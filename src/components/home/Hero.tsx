4a207ade50b48153b56370c215ba5aeabb6379dc
Developer Sam <developer.sami44@gmail.com>
2025-09-21 10:02:08 +0000

    reverse the changes int not i need

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -78,7 +78,7 @@ export function Hero() {
                   alt={aboutData.name}
                   data-ai-hint={profilePic.imageHint}
                   fill
-                  className="object-cover mix-blend-plus-lighter"
+                  className="object-cover"
                   priority
                 />
               </div>
