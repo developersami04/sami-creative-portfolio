ac5a0818e6077bc18d133d83cfafa517838fb3d9
Developer Sam <developer.sami44@gmail.com>
2025-09-22 20:38:25 +0000

    ```1. https://res.cloudinary.com/dcfurdz3u/video/upload/v1758573236/Demo

--- src/lib/music-data.ts
+++ src/lib/music-data.ts
@@ -7,23 +7,28 @@ export type MusicTrack = {
 
 export const musicTracks: MusicTrack[] = [
   {
-    url: "https://res.cloudinary.com/content-storage/video/upload/v1756382941/mixkit-light-rain-loop-2393_jsitx0.wav",
-    title: "Light Rain",
-    artist: "Mixkit",
+    url: "https://res.cloudinary.com/dcfurdz3u/video/upload/v1758573236/Demon_Slayer__Infinity_Castle_OST___Zenitsu_7th_Form__Flaming_Thunder_God_Zenitsu_vs_Kaigaku_hvob5g.m4a",
+    title: "Zenitsu vs. Kaigaku",
+    artist: "Original Soundtrack",
   },
   {
-    url: "https://res.cloudinary.com/content-storage/video/upload/v1756382941/mixkit-light-rain-loop-2393_jsitx0.wav",
-    title: "Rainy Mood",
-    artist: "Mixkit",
+    url: "https://res.cloudinary.com/dcfurdz3u/video/upload/v1758573237/Demon_Slayer__Infinity_Castle_-_Tomioka_s_Demon_Slayer_Mark_Giyu_vs._Akaza___EPIC_VERSION_izyqc7.m4a",
+    title: "Giyu vs. Akaza",
+    artist: "Original Soundtrack",
   },
   {
-    url: "https://res.cloudinary.com/content-storage/video/upload/v1756382941/mixkit-light-rain-loop-2393_jsitx0.wav",
-    title: "Gentle Showers",
-    artist: "Mixkit",
+    url: "https://res.cloudinary.com/dcfurdz3u/video/upload/v1758573237/Muzan_vs_Hashira___Entrance_to_Infinity_Castle___Demon_Slayer_S4_E8___%E9%AC%BC%E6%BB%85%E3%81%AE%E5%88%83_OST_qo1vhs.m4a",
+    title: "Muzan vs. Hashira",
+    artist: "Original Soundtrack",
   },
   {
-    url: "https://res.cloudinary.com/content-storage/video/upload/v1756382941/mixkit-light-rain-loop-2393_jsitx0.wav",
-    title: "Peaceful Drizzle",
-    artist: "Mixkit",
+    url: "https://res.cloudinary.com/dcfurdz3u/video/upload/v1758573238/Demon_Slayer__Infinity_Castle_Shinobu_vs_Doma___Dance_of_the_Centipede___EPIC_VERSION_eyu0tu.m4a",
+    title: "Shinobu vs. Doma",
+    artist: "Original Soundtrack",
+  },
+  {
+    url: "https://res.cloudinary.com/dcfurdz3u/video/upload/v1758573238/Demon_Slayer__Infinity_Castle___Akaza_vs_Giyu_Tanjiro_EPIC_VERSION_cmuta7.m4a",
+    title: "Akaza vs. Giyu & Tanjiro",
+    artist: "Original Soundtrack",
   },
 ];
